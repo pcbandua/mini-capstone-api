7a625244da7c11af77d4ba0d20261116f9d524fe
Paulo Bandua <pcbandua@gmail.com>
2024-09-01 15:36:00 -0700

    jbuilder update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,15 @@
 class ProductsController < ApplicationController
+  def index
+    @Products = Product.all
+    render template: "products/index"
+  end
+
+  def show
+    # send an id in params
+    # p params[:id]
+    @product = Product.find_by(id: params[:id])
+    # use that id to find a recipe
+    # render that recipe
+    render template: "products/show"
+  end
 end
