913b41d0dcb05523e410352dbb5b0bf86c77449a
Paulo Bandua <pcbandua@gmail.com>
2024-09-04 22:59:33 -0700

    show action

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -26,6 +26,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
       assert_response 200
     end
   end
+
+  test "show" do
+    get "/products/#{Photo.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["id", "name", "price", "image_url", "description"]
+  end
 end
 
 # name, price (integer), image_url, and description.
