16732542ce25780f6c58f688c062092c85ae6591
Paulo Bandua <pcbandua@gmail.com>
2024-09-04 19:23:09 -0700

    fixed tests

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,19 @@
 class ProductsController < ApplicationController
   def index
-    @Products = Product.all
+    @products = Product.all
     render template: "products/index"
+  end
+
+  # name, price (integer), image_url, and description.
+  def create
+    @product = Product.create(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description_url],
+
+    )
+    render :show
   end
 
   def show
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,5 +1,13 @@
 require "test_helper"
 
+# create_table "products", force: :cascade do |t|
+#   t.string "name"
+#   t.integer "price"
+#   t.string "image_url"
+#   t.text "description"
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "index" do
     get "/products.json"
@@ -7,5 +15,17 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     data = JSON.parse(response.body)
     assert_equal Product.count, data.length
+  end
+end
+
+# name, price (integer), image_url, and description.
+test "create" do
+  assert_difference "Product.count", 1 do
+    post "/product.json", params: {
+                            name: "Glasses",
+                            price: 90,
+                            image_url: "http://www.google.com", description: "helloooooooooo",
+                          }
+    assert_response 200
   end
 end
