fac6a87f428aebbe7d4fdd36d38ece64ad23cf83
Paulo Bandua <pcbandua@gmail.com>
2024-09-10 13:58:43 -0700

    approved supplie_id added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_10_021537) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_10_205312) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_10_021537) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "supplier_id"
   end
 
   create_table "suppliers", force: :cascade do |t|
